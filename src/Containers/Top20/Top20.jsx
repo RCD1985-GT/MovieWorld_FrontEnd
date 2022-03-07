058a3e2f14711e233a4a22212dab4bd03d3d0fe7
RCD1985-GT <crazyspicefactory@gmail.com>
2022-03-07 20:52:46 +0100

    implemento vista Top20

--- src/Containers/Top20/Top20.jsx
+++ src/Containers/Top20/Top20.jsx
@@ -4,7 +4,12 @@ import './Top20.css';
 const Top20 = () => {
     return(
         <div className="diseñoTop20">
-            
+               <div className="contenedorTop20">
+                   <div className="contenedor1-5"> Top 1 a 5</div>
+                   <div className="contenedor6-10">Top 6 a 10</div>
+                   <div className="contenedor11-15"> Top 11 a 15</div>
+                   <div className="contenedor16-20"> Top 16 a 20</div>
+               </div>
         </div>
     );
 };
