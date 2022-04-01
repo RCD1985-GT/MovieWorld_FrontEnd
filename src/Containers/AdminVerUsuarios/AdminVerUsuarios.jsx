e183f649eff9cb7e56ca7398c5943b62495da79e
Rodrigo <infohotsmoke@gmail.com>
2022-04-01 12:43:55 +0200

    vista Trae usuarios funcionando

--- src/Containers/AdminVerUsuarios/AdminVerUsuarios.jsx
+++ src/Containers/AdminVerUsuarios/AdminVerUsuarios.jsx
@@ -4,30 +4,28 @@ import axios from 'axios';
 import { connect } from 'react-redux';
 
 const AdminVerUsuarios = (props) => {
-
+console.log("entro en AdminVerUsuarios")
     // Hook
     const [usuarios, setUsuarios] = useState([]);
 
     // UseEffect de montaje
     useEffect(() => {
-        traeUsuarios(); // PORQUE ESTA LINEA DE AQUI¿?
+        traeUsuarios(); 
     }, []);
 
     // UseEffect de actualizacion
-    useEffect(() => {
+    // useEffect(() => {
         
-    });
+    // });
 
     // Funcion traer usuarios
-    const traeUsuarios = async (props) => {
-
-        let config = {
-            headers: { Authorization: `Bearer ${props.credenciales.token}` }
-        };
-
-
+    const traeUsuarios = async () => {
+console.log("entro en funcion trae usuarios")
+      
         try {
-            let resultado = await axios.get("http://localhost:3300/usuarios", config);
+            console.log("llamo a axios")
+            let resultado = await axios.get("http://localhost:3300/usuarios");
+            console.log("llamada a axios realizada")
             console.log(resultado);
             setUsuarios(resultado.data); // SE GUARDA EL RESULTADO EN EL HOOK
 
@@ -44,10 +42,10 @@ const AdminVerUsuarios = (props) => {
 
                 {usuarios.map(usuario => {
 
-                    return (
-                        <div className="itemUsuario" key={usuario.id} >
-                            <p className="usuario">{usuario.nombre}</p>
-                            <p className="usuario">{usuario.apellido}</p>
+                    return ( // COMPROBAR SI USUARIOS.ID VA EN SINGULAR O PLURAL
+                        <div className="itemUsuario" key={usuarios.id} > 
+                            <p className="usuario">{usuarios.nombre}</p>
+                            <p className="usuario">{usuarios.apellido}</p>
                             <p className="usuario">{usuario.email}</p>
                         </div>
                     )
